516b1074ff54b9802cfc5cc2f4cab8870daa7d46
tomoki katsu <stiino58@gmail.com>
2017-08-26 18:01:15 +0900

    さらにフォルダ構成変更

---
+++ TodoList/Model/DB/TodoObject.h
@@ -0,0 +1,13 @@
+//
+//  TodoObject.h
+//  TodoList
+//
+//  Created by tomoki on 2017/08/21.
+//  Copyright © 2017年 pingmayong. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface TodoObject : RLMObject
+
+@end
---
+++ TodoList/Model/DB/TodoObject.m
@@ -0,0 +1,13 @@
+//
+//  TodoObject.m
+//  TodoList
+//
+//  Created by tomoki on 2017/08/21.
+//  Copyright © 2017年 pingmayong. All rights reserved.
+//
+
+#import "TodoObject.h"
+
+@implementation TodoObject
+
+@end
